6a09d366e5f49fd70fa3f5a7f62ab5491c0d07a2
ginitachula <86498552+ginitachula@users.noreply.github.com>
2022-04-17 00:55:50 -0400

    add

--- Desarrollo Social/src/Modelos/Afiliado.java
+++ Desarrollo Social/src/Modelos/Afiliado.java
@@ -1,5 +1,42 @@
 package Modelos;
 
 public class Afiliado {
-    carlito brinda las cachapas 
+
+    private String
+            id_afi,
+            Fundacion,
+            Beneficiario;
+
+    private char estatus_afi;
+    
+    public Afiliado (String id_afi, String Fundacion, String Beneficiario, char estatus_afi){
+        this.id_afi=id_afi;
+        this.Fundacion=Fundacion;
+        this.Beneficiario=Beneficiario
+        this.estatus=estatus;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public char getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(char estatus) {
+        this.estatus = estatus;
+    }
 }
