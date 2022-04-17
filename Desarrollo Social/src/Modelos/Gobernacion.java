a619b6246f1458a23a1762798f3caf872d114e27
Mariel <97931349+agbmariel@users.noreply.github.com>
2022-04-17 21:32:06 +0200

    hhhhhh

--- Desarrollo Social/src/Modelos/Gobernacion.java
+++ Desarrollo Social/src/Modelos/Gobernacion.java
@@ -1,5 +1,20 @@
 package Modelos;
 
 public class Gobernacion {
-    
-}
+    private string 
+            rifGobernacion,
+            razonSocial;
+    private char estatusGob;
+
+    public Gobernacion(string rifGobernacion, char estatusGob){
+        this.rifGobernacion = rifGobernacion;
+        this.estatusGob = estatusGob;
+    }
+
+    public  string getRifGobernacion(){
+        return rifGobernacion;
+    }
+
+    public void setRifGobernacion(string rifGobernacion){
+        this.rifGobernacion = rifGobernacion;
+    }
