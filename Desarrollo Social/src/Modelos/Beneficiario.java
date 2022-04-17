949d1d62353008e28694a2a9d5f3ffc3bbfdf39e
Mariel <97931349+agbmariel@users.noreply.github.com>
2022-04-17 06:59:44 +0200

    aaaaaaaaaaaaaaaaaaaaaaaa

--- Desarrollo Social/src/Modelos/Beneficiario.java
+++ Desarrollo Social/src/Modelos/Beneficiario.java
@@ -1,5 +1,27 @@
 package Modelos;
 
-public class Beneficiarios {
-    
-}
+public class Beneficiario {
+    private date
+        fechaNac;
+    private char estatusBe;
+
+    public Beneficiario(date fechaNac, char estatus){ 
+        this.fechaNac=fechaNac;
+        this.estatus=estatus;
+    }
+
+    public date getFechaNac(){ 
+        return fechaNac;
+     }
+
+    public void setFechaNac(date fechaNac){
+        this.fechaNac=fechaNac;
+    }
+
+    public char getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(char estatus) {
+        this.estatus = estatus;
+    }
